5774165d7c9070360f5de0e7469054a1b9f68407
Lekan Osindeinde <osindeindelekan@gmail.com>
2022-05-14 16:41:41 +0100

    Node js MongoDB models

---
+++ models/User.js
@@ -0,0 +1,17 @@
+const { timeStamp } = require("console");
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: treue },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User, UserSchema");
